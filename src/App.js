18610e03649afde62ea8f05e3f773c5e8075fc2d
sharafath ali <sharafathaliofficial@gmail.com>
2024-04-26 00:32:30 +0530

    tailwind added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ring-offset-blue-900 from-fuchsia-500 hover:bg-black">
+     re
     </div>
   );
 }
