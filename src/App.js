33251c6451f1178ccb40bfd297452f8405a40117
sharafath ali <sharafathaliofficial@gmail.com>
2024-05-01 00:15:45 +0530

    started other parts

--- src/App.js
+++ src/App.js
@@ -1,32 +1,9 @@
+import "./App.css";
+import Header from "./components/Header";
 function App() {
   return (
     <>
-      <div className="flex justify-between bg-stone-500 p-10">
-        <div className="flex justify-between">
-          <h1 className="font-mono from-neutral-700 font-black">
-            Sharafath Ali
-          </h1>
-          <p className="font-mono from-neutral-700 font-black px-4">/</p>
-          <p className="font-mono from-neutral-700 font-black">
-              Associate Software Developer
-          </p>
-        </div>
-        <div className="flex justify-between">
-          <div>
-             <p className="font-mono from-neutral-600 font-black px-4 hover:bg-slate-100">ABOUT ME</p>
-          </div>
-          <div>
-            <p className="font-mono from-neutral-700 font-black px-4">RESUME</p>
-          </div>
-          <div>
-            <p className="font-mono from-neutral-700 font-black px-4">PROJECTS</p>
-          </div>
-          <div>
-            <p className="font-mono from-neutral-700 font-black px-4">CONTACT</p>
-          </div>
-        </div>
-      </div>
-      <div className="bg-slate-50">fufy</div>
+      <Header />
     </>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="flex justify-between bg-stone-500 p-10 items-center">
+      <div className="flex justify-between">
+        <h1 className="font-mono from-neutral-700 font-black  text-xl">
+          Sharafath Ali
+        </h1>
+        <p className="font-mono from-neutral-700 font-black px-4  text-xl">/</p>
+        <p className="font-mono from-neutral-700 font-black  text-xl">
+          Associate Software Developer
+        </p>
+      </div>
+      <div className="flex justify-between items-center">
+        <div>
+          <p className="font-mono from-neutral-600 font-black px-4 hover:bg-slate-100">
+            ABOUT ME
+          </p>
+        </div>
+        <div>
+          <p className="font-mono from-neutral-700 font-black px-4">RESUME</p>
+        </div>
+        <div>
+          <p className="font-mono from-neutral-700 font-black px-4">PROJECTS</p>
+        </div>
+        <div>
+          <p className="font-mono from-neutral-700 font-black px-4">CONTACT</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
