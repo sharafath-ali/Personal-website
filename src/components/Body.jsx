5c8ed9166e16166759a89def8c3e7218892c91f1
sharafath ali <sharafathaliofficial@gmail.com>
2024-05-05 01:04:19 +0530

    added a button s under body and material ui to the project

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import Button from "@mui/material/Button";
+import WorkOutlineIcon from "@mui/icons-material/WorkOutline";
+import DownloadIcon from "@mui/icons-material/Download";
 
 function Body() {
   return (
-    <div>
-
+    <div className="flex justify-between">
+      <div style={{ width: "50%" }} className="flex">
+        <div>
+            
+        </div>
+      </div>
+      <div style={{ width: "50%" }}>
+        <Button variant="outlined" size="small" startIcon={<WorkOutlineIcon />}>
+          Projects
+        </Button>
+        <Button variant="outlined" size="small" endIcon={<DownloadIcon />}>
+          Resume
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Body
+export default Body;
