24175c0755d63b2e070ffb36a11d721890099686
sharafath ali <sharafathaliofficial@gmail.com>
2024-05-24 00:49:18 +0530

    issue fixed

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,7 +4,7 @@ function Resume() {
   return (
     <div className="h-dvh">
       <div className="flex justify-center items-center py-5 text-4xl font-black">
-        Projects
+        Resume
       </div>
       <div className="border-solid border-2 border-slate-600 flex justify-center items-center py-3 my-4">
         ssdsf
