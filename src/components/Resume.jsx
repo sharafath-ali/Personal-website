785d47d994869abfcb2e8e6671f8acdbb2bc16d4
sharafath ali <sharafathaliofficial@gmail.com>
2024-07-04 23:52:41 +0530

    edited the resume section

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "../Css/Resume.css";
+
 function Resume() {
   return (
-    <div className="resume-page" style={{ fontFamily: "'Poppins', sans-serif" }}>
+    <div
+      className="resume-page"
+      style={{ fontFamily: "'Poppins', sans-serif" }}
+    >
       <h2>My Resume</h2>
       <div className="resume-section">
         <h3>Experience</h3>
@@ -10,14 +14,13 @@ function Resume() {
           <div className="card">
             <h4>Associate Software Engineer</h4>
             <p>Global Infonet Inc, Kochi</p>
-            <p>September 2023- Present (10 months)</p>
+            <p>September 2023 - Present (10 months)</p>
           </div>
           <div className="card">
-            <h4>Associate Software Engineer trainee</h4>
+            <h4>Associate Software Engineer Trainee</h4>
             <p>Global Infonet Inc, Kochi</p>
             <p>March 2023 - September 2023 (5 months)</p>
           </div>
-          {/* Add more experience cards as needed */}
         </div>
       </div>
 
@@ -28,34 +31,48 @@ function Resume() {
             <h4>Bachelor of Computer Application</h4>
             <p>Jamia Hamdard University, Delhi</p>
             <p>Year of Graduation: 2022</p>
-            <p></p>
           </div>
           <div className="card">
-            <h4>12 th (Higher Secondary Education)</h4>
+            <h4>12th (Higher Secondary Education)</h4>
             <p>G.V.H.S. School, Kadirur, Kannur, Kerala</p>
             <p>Kerala State Board - 2017 - 2019</p>
-            <p>Details about the grade...</p>
           </div>
           <div className="card">
-            <h4>10 th (S S L C)</h4>
+            <h4>10th (SSLC)</h4>
             <p>Kuthuparamba High School, Kannur, Kerala</p>
             <p>Kerala State Board - 2017</p>
-            <p>Details about the grade...</p>
           </div>
         </div>
       </div>
 
       <div className="resume-section">
         <h3>Skills</h3>
-        <div className="resume-cards">
-          <div className="card">
-            <h4>Programming Languages</h4>
-            <p>JavaScript, Python, etc.</p>
-          </div>
-          <div className="card">
-            <h4>Technologies</h4>
-            <p>React, Node.js, MongoDB, etc.</p>
-          </div>
+        <div className="skills-grid">
+          {[
+            "React.Js",
+            "Node (Node Js)",
+            "Express (Express Js)",
+            "SHARE POINT FRAMEWORK",
+            "JavaScript",
+            "TypeScript",
+            "Redux-Toolkit",
+            "MongoDB",
+            "Material-UI, Chakra-UI, React-Bootstrap",
+            "Git",
+            "Postman",
+            "Azure Devops",
+            "GitHub",
+            "React Router",
+            "Styled Components - Sass",
+            "Microsoft Windows Series, Linux",
+            "HTML, CSS",
+            "Problem-Solving",
+            "Strong Communication",
+          ].map((skill) => (
+            <div key={skill} className="skill-chip">
+              {skill}
+            </div>
+          ))}
         </div>
       </div>
 
