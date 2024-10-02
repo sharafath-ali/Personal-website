b16ff0809ae5c8106c44b751af6304e18b197d1a
sharafath ali <sharafathaliofficial@gmail.com>
2024-10-03 01:16:06 +0530

    ui updated in resume part

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,26 +1,29 @@
 import React from "react";
 import "../Css/Resume.css";
 import resumePDF from "../Assets/resume.pdf";
+import { Skills } from "../consts/skills";
 
 function Resume() {
   return (
     <div
-      className="resume-page"
-      style={{ fontFamily: "'Poppins', sans-serif" }}
-    >
+     className="resume-page"
+     style={{ fontFamily: "'Poppins', sans-serif" }}
+     >
       <h2>My Resume</h2>
       <div className="resume-section">
         <h3>Experience</h3>
         <div className="resume-cards">
-          <div className="card">
-            <h4>Associate Software Engineer</h4>
-            <p>Global Infonet Inc, Kochi</p>
-            <p>September 2023 - October 2024 (1 year and 1 month)</p>
+          <div className="card experience-card">
+            <h4 className="job-title">Associate Software Engineer</h4>
+            <p className="company-name">Global Infonet Inc, Kochi</p>
+            <p className="employment-dates">Sept 2023 - Oct 2024</p>
+            <p className="employment-duration">(1 yr 1 mo)</p>
           </div>
-          <div className="card">
-            <h4>Associate Software Engineer Trainee</h4>
-            <p>Global Infonet Inc, Kochi</p>
-            <p>March 2023 - September 2023 (5 months)</p>
+          <div className="card experience-card">
+            <h4 className="job-title">Associate Software Engineer Trainee</h4>
+            <p className="company-name">Global Infonet Inc, Kochi</p>
+            <p className="employment-dates">Mar 2023 - Sept 2023</p>
+            <p className="employment-duration">(5 mo)</p>
           </div>
         </div>
       </div>
@@ -29,19 +32,19 @@ function Resume() {
         <h3>Education</h3>
         <div className="resume-cards">
           <div className="card">
-            <h4>Bachelor of Computer Application</h4>
-            <p>Jamia Hamdard University, Delhi</p>
-            <p>Year of Graduation: 2022</p>
+            <h4 className="job-title">Bachelor of Computer Application</h4>
+            <p className="company-name">Jamia Hamdard University, Delhi</p>
+            <p className="employment-dates">Year of Graduation: 2022</p>
           </div>
           <div className="card">
-            <h4>12th (Higher Secondary Education)</h4>
-            <p>G.V.H.S. School, Kadirur, Kannur, Kerala</p>
-            <p>Kerala State Board - 2017 - 2019</p>
+            <h4 className="job-title">12th (Higher Secondary Education)</h4>
+            <p className="company-name">G.V.H.S. School, Kadirur, Kannur, Kerala</p>
+            <p className="employment-dates">Kerala State Board - 2017 to 2019</p>
           </div>
           <div className="card">
-            <h4>10th (SSLC)</h4>
-            <p>Kuthuparamba High School, Kannur, Kerala</p>
-            <p>Kerala State Board - 2017</p>
+            <h4 className="job-title">10th (SSLC)</h4>
+            <p className="company-name">Kuthuparamba High School, Kannur, Kerala</p>
+            <p className="employment-dates">Kerala State Board - 2017</p>
           </div>
         </div>
       </div>
@@ -49,32 +52,7 @@ function Resume() {
       <div className="resume-section">
         <h3>Skills</h3>
         <div className="skills-grid">
-          {["React.Js",
-            "Node (Node Js)",
-            "Express (Express Js)",
-            "JavaScript",
-            "TypeScript",
-            "MongoDB",
-            "Mongoose",
-            "Git",
-            "Postman",
-            "Redux-Toolkit",
-            "jsonwebtoken (JWT-based authentication)",
-            "bcrypt (Secure password hashing)",
-            "REST APIs",
-            "Model–View–Controller (MVC)",
-            "Azure DevOps",
-            "Socket.IO",
-            "SHARE POINT FRAMEWORK",
-            "Server-Side Rendering (SSR) with EJS",
-            "GitHub",
-            "React Router",
-            "Material-UI, Chakra-UI, React-Bootstrap",
-            "Styled Components - Sass",
-            "Microsoft Windows Series, Linux",
-            "HTML, CSS",
-            "Problem-Solving",
-            "Strong Communication"].map((skill) => (
+          {Skills.map((skill) => (
             <div key={skill} className="skill-chip">
               {skill}
             </div>
@@ -92,3 +70,4 @@ function Resume() {
 }
 
 export default Resume;
+
