9dbc58a574923ba64df434a156d858177faabc88
Sharafath Ali <sharafathaliofficial@gmail.com>
2026-03-25 00:49:36 +0530

    refactor: Migrate project from Create React App to Vite, adding new configuration files and updating package dependencies.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import "./App.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import Body from "./components/Body";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Projects from "./components/Projects";
